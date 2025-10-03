2e792e206f15a0e21908afce17b78fa0e5d82fc5
Nathan Osti <nathanosti.job@gmail.com>
2025-10-03 16:47:58 -0300

    fix: lsp's warnings

--- .config/nvim/lua/plugins/java.lua
+++ .config/nvim/lua/plugins/java.lua
@@ -1,11 +1,16 @@
 return {
   {
     "mfussenegger/nvim-jdtls",
+    ft = { "java" },
     config = function()
-      require("jdtls").start_or_attach({ -- config padrão
-        cmd = { "java-lsp.sh", "jdtls" },
-        root_dir = vim.fn.getcwd(),
-      })
+      local jdtls_path = vim.fn.stdpath("data") .. "/mason/bin/jdtls"
+      local root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" })
+
+      local config = {
+        cmd = { jdtls_path },
+        root_dir = root_dir,
+      }
+      require("jdtls").start_or_attach(config)
     end,
   },
 }
